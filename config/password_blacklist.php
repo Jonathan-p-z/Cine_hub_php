ae2e4f358a7823e8f8aed1018048ac9a38450a91
pjonatha <jonathan.perez@ynov.com>
2026-02-11 02:23:32 +0100

    securite: logs + anti-bruteforce + sessions db

---
+++ config/password_blacklist.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+if (!defined('BASE_PATH')) {
+    http_response_code(403);
+    exit('Forbidden');
+}
+
+return [
+    'password',
+    'password1',
+    'password123',
+    '12345678',
+    '123456789',
+    'qwerty',
+    'azerty',
+    'letmein',
+    'welcome',
+    'admin',
+    'administrator',
+    'iloveyou',
+    'monkey',
+    'dragon',
+    'football',
+    'baseball',
+    'sunshine',
+    'princess',
+    'trustno1',
+    'motdepasse',
+    'cinehub',
+];
